d04007c3810052e5544561d219cc7b4078a44257
Ricardo <ricardo240190@gmail.com>
2025-12-12 01:58:04 -0500

    Agregando logica de calculadora

---
+++ modules/calculadora.py
@@ -0,0 +1,9 @@
+class CalculadoraElectoral:
+    def __init__(self):
+        # Valor de la UIT 2025
+        self.UIT = 5150 
+
+    def calcular_deuda(self, es_miembro, voto, categoria):
+        # Lógica base (se ampliará luego)
+        deuda = 0.0
+        return deuda, []
